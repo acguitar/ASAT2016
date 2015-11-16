3e82b9639f30d7c8655168252a8b0ec09dcb0d96
Yehui Liu <yehuiliu1@gmail.com>
2015-11-15 18:30:08 -0600

    Revert "test"
    This reverts commit 9b9dad101ea774ca3cd517addf8c54b56b2467cd.

--- ASAT2016/MediaMangementLogin.cs
+++ ASAT2016/MediaMangementLogin.cs
@@ -73,6 +73,15 @@ namespace ASAT2016
 
         private void tbApiUrl_TextChanged(object sender, EventArgs e)
         {
+            int b;
+            int a;
+            int c;
+            int d;
+            int f;
+            int g; 
+            int fh; 
+            int fafd; 
+            int adfa; 
             int adfafa; 
             int afadfafa; 
             int afadafadfadsf;
